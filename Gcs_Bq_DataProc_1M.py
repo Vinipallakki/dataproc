b089b60d714c5f4df76ee1924bbe72b20a5a95ce
pallakki <98947934+Vinipallakki@users.noreply.github.com>
2025-03-04 12:11:04 +0530

    Update Gcs_Bq_DataProc_1M.py

--- Gcs_Bq_DataProc_1M.py
+++ Gcs_Bq_DataProc_1M.py
@@ -60,6 +60,29 @@ print("Data successfully written to BigQuery!")
 
 # Stop Spark session
 spark.stop()
+#===========================================
+from pyspark.sql import SparkSession
+
+# Initialize Spark Session
+spark = SparkSession.builder.appName("EcommerceSalesAnalysis").getOrCreate()
+
+# Read CSV file from GCS
+df = spark.read.csv("gs://vini123654789/sample_large_dataset.csv", header=True, inferSchema=True)
+
+# Apply filter directly in PySpark
+filtered_df = df.filter(df.age > 20)
+
+# Write output to BigQuery
+filtered_df.write \
+    .format("bigquery") \
+    .option("temporaryGcsBucket", "vini123654789") \
+    .mode("overwrite") \
+    .save("nimble-courier-449405-f7.first.three")
+
+print("Data successfully written to BigQuery!")
+
+# Stop Spark session
+spark.stop()
 
 
 #gcloud dataproc jobs submit pyspark gs://vini123654789/main.py     --cluster=my-dataproc-cluster     --region=us-central1     --properties spark.executor.memory=4g,spark.executor.cores=2     --jars gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar
