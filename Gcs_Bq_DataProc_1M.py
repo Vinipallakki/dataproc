b6e894de8652215e565db8797a96356c7ded56ed
pallakki <98947934+Vinipallakki@users.noreply.github.com>
2025-03-04 11:57:29 +0530

    Update Gcs_Bq_DataProc_1M.py

--- Gcs_Bq_DataProc_1M.py
+++ Gcs_Bq_DataProc_1M.py
@@ -29,3 +29,36 @@ print("Data successfully written to BigQuery!")
 # Stop Spark session
 spark.stop()
 
+#====================================================
+from pyspark.sql import SparkSession
+
+# Initialize Spark Session
+spark = SparkSession.builder.appName("EcommerceSalesAnalysis").getOrCreate()
+
+# Read CSV file from GCS
+df = spark.read.csv("gs://vini123654789/sample_large_dataset.csv", header=True, inferSchema=True)
+
+# Register DataFrame as SQL Table
+df.createOrReplaceTempView("sales")
+
+# Perform SQL Query
+result_df = spark.sql("""
+    SELECT *
+    FROM sales
+    WHERE age > 20
+""")
+
+# Write output to BigQuery
+result_df.write \
+    .format("bigquery") \
+    .option("table", "nimble-courier-449405-f7.first.three") \
+    .option("temporaryGcsBucket", "nimble-courier-449405-f7") \
+    .mode("overwrite") \
+    .save()
+
+print("Data successfully written to BigQuery!")
+
+# Stop Spark session
+spark.stop()
+
+
